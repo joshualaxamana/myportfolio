38dc614773a82caddfbfc7b29ab870998a628b03
Joshua Laxamana <joshlaxamana@gmail.com>
2019-11-06 03:23:20 +0800

    animation on homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import logo from './logo.png';
 import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom';
+import Typical from 'react-typical';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
@@ -12,7 +13,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Joshua Laxamana</h1>
-        <h3>Software Developer</h3>
+        <Typical
+          steps={['Software', 1000, 'Software Developer', 2000]}
+          loop={Infinity}
+          wrapper="h3"
+        />
       </header>
     </div>
   );
