5a6427180b5c39930ba82546abc627bb07753bca
Joshua Laxamana <joshlaxamana@gmail.com>
2019-11-06 02:55:06 +0800

    Updates

---
+++ precache-manifest.cfe949501c1a4ea029bc41e58bd9764e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7e6c062aad0af98689abac79c1c409",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "b4fe894a8c9282b5d7a2",
+    "url": "/myportfolio/static/css/main.e10e5c6e.chunk.css"
+  },
+  {
+    "revision": "dbfe4be83b0118f45334",
+    "url": "/myportfolio/static/js/2.29687cc9.chunk.js"
+  },
+  {
+    "revision": "b4fe894a8c9282b5d7a2",
+    "url": "/myportfolio/static/js/main.cf800336.chunk.js"
+  },
+  {
+    "revision": "f6da0988cc920d9604bb",
+    "url": "/myportfolio/static/js/runtime-main.4c1d368d.js"
+  },
+  {
+    "revision": "e676d681849913ef1aaaebeef376a68c",
+    "url": "/myportfolio/static/media/logo.e676d681.png"
+  }
+]);
